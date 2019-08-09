72370ab91dc91598fd6ba4d43e3e354cad53aca8
mercer <784980118@qq.com>
2019-08-10 00:45:39 +0800

    功能基本实现了，最后一波就是修改样式了

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,29 +1,46 @@
 import express from 'express';
 import db from "./../lib/DbHelper";
 import fs from "fs";//filesystem
+import jwt from "jsonwebtoken";
+// import async1 from "async";
 
 let router = express.Router();
 
-
+global.keys = "mercer";
 //登录接口
-router.post("/login",(req,res,next)=>{
-    let sql ="SELECT `uid`,`uname` FROM user_infor WHERE `uname`= ? AND `upassword`=?;";
-    let params =[req.body.uname,req.body.upassword];
+router.post("/login", (req, res, next) => {
+    let sql = "SELECT `uid`,`uname` FROM user_infor WHERE `uname`='"+req.body.uname+"' AND `upassword`='"+req.body.upassword+"';";
+    //let params = [req.body.uname, req.body.upassword];
     //返回结果
+    console.log(sql);
+    
     var resultObj = {
-        "msg":"登录失败",
+        "msg": "登录错误",
         status: -1
     };
 
-    db.query(sql,params).then((result)=>{
-        console.log(result[0]);
-        if(result.length > 0){
+    db.query(sql,[]).then((result) => {
+        console.log(result);
+        if (result.length > 0){
+            //如果登录了，就保存到session中间
+            //如果登录成功了,就把当前登录者的信息保存到session
+            //服务端的页面（接口）与页面（接口）之间的传值方式
+            
+            var token = jwt.sign(Object.assign({},result[0]),global.keys,{
+                expiresIn:60*30
+            });
+            console.log("输出token:"+token)
+            //req.session["userInfo"] = result[0];
             resultObj.data = result[0];
-            resultObj.msg="登录成功";
-            resultObj.status=1;
-        }
-        res.json(resultObj);
-    }).catch((err)=>{
+            resultObj.msg = "登录成功";
+            resultObj.status = 1;
+            resultObj.token = token;
+           
+            // res.status(200).json(resultObj);
+            res.json(resultObj);
+        }  
+        
+    }).catch((err) => {
         console.log("error");
         resultObj.err = err;
         res.json(resultObj);
@@ -31,51 +48,176 @@ router.post("/login",(req,res,next)=>{
 })
 
 //注册接口
-router.post("/reg",(req,res,next)=>{
+router.post("/reg", (req, res, next) => {
     let sql = "INSERT INTO user_infor (uname,upassword,uemail) VALUES(?,?,?);";
     let params = [
         req.body.uname,
         req.body.upassword,
         req.body.uemail
     ];
-    db.queryAsync(sql,params).then((data)=>{
-        if(data.affectedRows ==1){
-            res.json({"msg":"注册成功",status:1})
-        }else{
-            res.json({"msg":"注册失败",status:-1})
+    db.queryAsync(sql, params).then((data) => {
+        if (data.affectedRows == 1) {
+            res.json({ "msg": "注册成功", status: 1 })
+        } else {
+            res.json({ "msg": "注册失败", status: -1 })
         }
         //console.log(data)
-    },(err)=>{
-        res.json({"msg":"注册失败",status:-2,err})
+    }, (err) => {
+        res.json({ "msg": "注册失败", status: -2, err })
     });
 });
 
-router.get("/goods",(req,res,next)=>{
-//fs模块
 
-//同步方法，返回结果
-    var tmpPid = req.query.pid==undefined?0:req.query.pid;//接受前端发过来的url带参数得到它的参数
-    var goodsStr = fs.readFileSync("./data/goodsList.json",{
-        encoding:"utf-8",  //返回的编码就是utf-8格式
-        flag:"r"
+//产品列表
+router.get("/goods", (req, res, next) => {
+    //fs模块
+
+    //同步方法，返回结果
+    var tmpPid = req.query.pid == undefined ? 0 : req.query.pid;//接受前端发过来的url带参数得到它的参数
+    var goodsStr = fs.readFileSync("./data/goodsList.json", {
+        encoding: "utf-8",  //返回的编码就是utf-8格式
+        flag: "r"
     });
     // console.log(JSON.parse(goodsStr));
     const goodsList = JSON.parse(goodsStr);
-    var queryList =  goodsList.filter((el)=>{
+    var queryList = goodsList.filter((el) => {
         return el.pId.indexOf(tmpPid) != -1;
     })
-    if(req.query.pid == undefined){
+    if (req.query.pid == undefined) {
         res.json(goodsList);
-    }else{
+    } else {
         res.json(queryList);
     }
-    
-//异步方法，用回调函数
+
+    //异步方法，用回调函数
     // var goodsStr = fs.readFile("./../data/goodsList.json",function(err,data){
     //     console.log(data)
     // })
 })
 
-module.exports={
+//加入购物车
+router.post("/addcart", async (req, res, next) => {
+
+
+    //一.查询
+    //1.sql
+    const selectSQL = "SELECT * FROM carts WHERE pId=? AND uid=?";
+    var selectParams = [
+        req.body.pId,
+        req.body.uId
+    ]
+    let queryData = await db.queryAsync(selectSQL, selectParams)
+
+
+    if (queryData.length >= 1) {
+        //三.修改
+        var updateSQL = "UPDATE carts SET pNum=pNum+?,pTotal=pNum*pPrice WHERE uid=? AND pId=?";
+        var updateParams = [
+            req.body.pNum,
+            req.body.uId,
+            req.body.pId
+        ]
+        let updateData = await db.queryAsync(updateSQL, updateParams)
+        if (updateData.affectedRows >= 1) {
+            res.json({
+                msg: "加入成功u",
+                status: 1
+            })
+        } else {
+            res.json({
+                msg: "加入失败u",
+                status: -1
+            })
+        }
+    } else {
+        //二.插入
+        var insertSQL = "INSERT INTO carts (`uid`,`pId`,`pName`,`pPrice`,`pNum`,`pTotal`,`pImg`) VALUES (?,?,?,?,?,?,?);";
+        var insertParams = [
+            req.body.uId,
+            req.body.pId,
+            req.body.pName,
+            req.body.pPrice,
+            req.body.pNum,
+            req.body.pTotal,
+            req.body.pImg
+        ]
+        console.log(insertParams)
+        let insertData = await db.queryAsync(insertSQL, insertParams)
+        if (insertData.affectedRows >= 1) {
+            res.json({
+                msg: "加入成功i",
+                status: 2
+            })
+        } else {
+            res.json({
+                msg: "加入失败i",
+                status: -2
+            })
+        }
+    }
+
+
+
+
+
+    //如果大于1，表示已经购买商品，只需要修改数量即可
+    //否则插入一条新的数据
+
+    //安装组件 cnpm i async --save-dev
+    //2.params
+
+
+
+
+
+})
+
+router.get("/getcart", async (req, res, next) => {
+
+    let uid = req.query.uId;
+
+    let sql = "SELECT * FROM carts WHERE uid = ?;";
+
+    let params = [uid];
+
+    const result = await db.query(sql, params);
+    res.json(result)
+    // console.log(result);
+
+})
+
+router.post("/modify", async (req, res, next) => {
+
+    let params = [
+        req.body.pNum,
+        req.body.uId,
+        req.body.pId,
+    ];
+
+    let sql = "UPDATE carts SET pNum =?,pTotal = pNum*pPrice WHERE uid=? AND pid=?;";
+
+
+
+    const result = await db.query(sql, params);
+    res.json(result)
+    // console.log(result);
+
+})
+//删除
+router.post("/delete", async (req, res, next) => {
+
+    let params = [
+        req.body.uId,
+        req.body.pId,
+    ];
+
+    let sql = "DELETE FROM carts WHERE uid=? AND pid=?;";
+
+    const result = await db.query(sql, params);
+    res.json(result)
+    // console.log(result);
+
+})
+module.exports = {
     router
 }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyparser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cookieSession from 'cookie-session';
+import jwt from 'jsonwebtoken';
 
 //引入路由管理
 import {router} from './routes/apiRouter';  //es6结构解析
@@ -21,13 +22,52 @@ app.use(cookieSession({
 //把public文件往外暴露了，人人都能访问public了
 app.use(express.static("./public"));
 
+var whiteList = ["/api/login","/api/reg","/api/goods","/api/checkname"];
 
 //拦截器
 app.all("*",(req,res,next)=>{
-    res.header("Content-Type","application/json;charset=utf-8");
+    // res.header("Content-Type","application/json;charset=utf-8");
     res.header("Access-Control-Allow-Origin","*");//后期删除,之后会使用代理
-
-    next();
+    res.header("Access-Control-Allow-Headers","token,x-requested-with,content-type");
+    res.header("X-Powered-By",'3.2.1');
+    // res.header("Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE,PUT");
+    // res.header('Access-Control-Allow-Headers:x-requested-with,content-type'); 
+    // console.log("输出"+req.originalUrl)
+    //有可能是post请求带来的
+    //有可能是get请求带来的
+    //有可能是头部带来的
+    let token = req.body.token || req.query.token || req.headers.token;
+    if(whiteList.indexOf(req.originalUrl)!=-1){
+        next();
+    }else{
+        jwt.verify(token,global.keys,function(err,data){
+            if(err){
+                res.json({
+                    msg:"token失效"
+                })
+                return;
+            }
+            //接收到的token校验通过
+            next();
+        })
+    }
+    //进行拦截
+    //console.log(req.originalUrl)
+    // if(whiteList.indexOf(req.originalUrl)!=-1){
+        //next();如果已经登录了，就可以访问其他的接口了
+        
+    // }else{
+    //     //否则不允许登录
+    //     if(req.session["userInfo"] != undefined){
+            
+    //     }
+    //     res.status(404).json(
+    //         {
+    //             "msg":"还没有登录吧?,请先登录",
+    //             status:-1
+    //         })
+    // }
+    
 })
 
 //配置路由
